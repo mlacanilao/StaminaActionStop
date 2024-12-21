304b3c1c0f1f9a7a14251578d93051a7ee35d171
Michael Lacanilao <michael.v.lacanilao@gmail.com>
2024-12-21 11:48:44 -0500

    Add check for ModOptions assembly to prevent reflection issues

--- StaminaActionStop/StaminaActionStop.cs
+++ StaminaActionStop/StaminaActionStop.cs
@@ -1,7 +1,8 @@
-﻿using BepInEx;
+﻿using System;
+using System.Linq;
+using BepInEx;
 using HarmonyLib;
 using StaminaActionStop.Config;
-using UnityEngine;
 
 namespace StaminaActionStop
 {
@@ -9,30 +10,58 @@ namespace StaminaActionStop
     {
         internal const string Guid = "omegaplatinum.elin.staminaactionstop";
         internal const string Name = "Stamina Action Stop";
-        internal const string Version = "1.0.1.0";
+        internal const string Version = "1.0.2.0";
         internal const string ModOptionsGuid = "evilmask.elinplugins.modoptions";
+        internal const string ModOptionsAssemblyName = "ModOptions";
     }
 
     [BepInPlugin(GUID: ModInfo.Guid, Name: ModInfo.Name, Version: ModInfo.Version)]
     internal class StaminaActionStop : BaseUnityPlugin
     {
+        internal static StaminaActionStop Instance { get; private set; }
+        
         private void Start()
         {
+            Instance = this;
+            
             StaminaActionStopConfig.LoadConfig(config: Config);
             
             Harmony.CreateAndPatchAll(type: typeof(Patcher));
             
+            if (IsModOptionsInstalled())
+            {
+                try
+                {
+                    UI.UIController.RegisterUI();
+                }
+                catch (Exception ex)
+                {
+                    Log(payload: $"An error occurred during UI registration: {ex.Message}");
+                }
+            }
+            else
+            {
+                Log(payload: "Mod Options is not installed. Skipping UI registration.");
+            }
+        }
+
+        private static void Log(object payload)
+        {
+            Instance.Logger.LogInfo(data: payload);
+        }
+        
+        private bool IsModOptionsInstalled()
+        {
             try
             {
-                UI.UIController.RegisterUI();
+                return AppDomain.CurrentDomain
+                    .GetAssemblies()
+                    .Any(predicate: assembly => assembly.GetName().Name == ModInfo.ModOptionsAssemblyName);
             }
-            catch (System.IO.FileNotFoundException)
+            catch (Exception ex)
             {
-                Debug.LogWarning("[Stamina Action Stop] Mod Options mod is not installed/enabled. Skipping UI registration.");
-            }
-            catch (System.Exception ex)
-            {
-                Debug.LogError($"[Stamina Action Stop] An unexpected error occurred: {ex.Message}");
+                Log(payload: $"Error while checking for Mod Options: {ex.Message}");
+                return false;
             }
         }
     }
